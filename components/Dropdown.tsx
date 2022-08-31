23e52b435fb91a95cb45428760c3f0f8507b542a
shubham <shubhamsaroder435@gmail.com>
2022-08-31 17:18:42 +0530

    redepolye please

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -1,21 +1,22 @@
-import { Menu, Transition } from "@headlessui/react";
-import { Fragment } from "react";
-import { ChevronDownIcon } from "@heroicons/react/solid";
-import { LogoutIcon } from "@heroicons/react/outline";
-import { signOut, useSession } from "next-auth/react";
+import { Menu, Transition } from '@headlessui/react'
+import { Fragment } from 'react'
+import { ChevronDownIcon } from '@heroicons/react/solid'
+import { LogoutIcon } from '@heroicons/react/outline'
+import { signOut, useSession } from 'next-auth/react'
+import { Session } from 'next-auth'
 
 export default function Dropdown() {
-  const { data: session } = useSession();
+  const { data: session }: any = useSession()
 
   return (
-    <Menu as="div" className="w-24 h-12 relative flex items-center">
-      <div className="w-full absolute right-1 group">
-        <Menu.Button className="flex items-center w-full px-4 py-3 text-sm font-medium text-white bg-[#1A1A1A] rounded-full hover:bg-[#3E3E3E]">
+    <Menu as="div" className="relative flex h-12 w-24 items-center">
+      <div className="group absolute right-1 w-full">
+        <Menu.Button className="flex w-full items-center rounded-full bg-[#1A1A1A] px-4 py-3 text-sm font-medium text-white hover:bg-[#3E3E3E]">
           <ChevronDownIcon className="h-6 text-[#686868]" aria-hidden="true" />
           <img
-            src={session.user.image}
+            src={session && session.user?.image}
             alt=""
-            className="rounded-full w-11 h-11 absolute -right-1 object-cover"
+            className="absolute -right-1 h-11 w-11 rounded-full object-cover"
           />
         </Menu.Button>
       </div>
@@ -28,17 +29,17 @@ export default function Dropdown() {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0 w-56 mt-24 origin-top-right bg-[#1A1A1A] divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <Menu.Items className="absolute right-0 mt-24 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-[#1A1A1A] shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
           <div className="px-1 py-1 ">
             <Menu.Item>
               {({ active }) => (
                 <button
                   className={`${
-                    active && "bg-white/10"
-                  } group flex rounded-md items-center w-full px-2 py-2 text-sm font-semibold tracking-wide text-white cursor-default`}
+                    active && 'bg-white/10'
+                  } group flex w-full cursor-default items-center rounded-md px-2 py-2 text-sm font-semibold tracking-wide text-white`}
                   onClick={() => signOut({ redirect: false })}
                 >
-                  <LogoutIcon className="w-5 h-5 mr-2" aria-hidden="true" />
+                  <LogoutIcon className="mr-2 h-5 w-5" aria-hidden="true" />
                   Log out
                 </button>
               )}
@@ -47,5 +48,5 @@ export default function Dropdown() {
         </Menu.Items>
       </Transition>
     </Menu>
-  );
+  )
 }
