a71f54d3c56b3417471c5dafef01b615bb6f3541
shubham <shubhamsaroder435@gmail.com>
2022-08-31 17:29:54 +0530

    env

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -2,16 +2,16 @@ import { useSession } from 'next-auth/react'
 import Head from 'next/head'
 import { useEffect, useState } from 'react'
 import { useRecoilState } from 'recoil'
-import SpotifyWebApi from "spotify-web-api-node";
+import SpotifyWebApi from 'spotify-web-api-node'
 import { playingTrackState } from '../atoms/playerAtom'
 import Body from './Body'
-import Player from './Player';
+import Player from './Player'
 import Right from './Right'
 import Sidebar from './Sidebar'
 
 const spotifyApi = new SpotifyWebApi({
   clientId: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID,
-});
+})
 
 const Dashboard = () => {
   const { data: session } = useSession()
@@ -20,6 +20,7 @@ const Dashboard = () => {
   const [showPlayer, setShowPlayer] = useState<boolean>(false)
   useEffect(() => {
     setShowPlayer(true)
+    console.log({ token: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID })
   }, [])
 
   const chooseTrack = (track: any) => {
@@ -28,12 +29,12 @@ const Dashboard = () => {
   return (
     <main className="flex min-h-screen min-w-max bg-black lg:pb-24">
       <Sidebar />
-      <Body  spotifyApi={spotifyApi} chooseTrack={chooseTrack} />
+      <Body spotifyApi={spotifyApi} chooseTrack={chooseTrack} />
       <Right spotifyApi={spotifyApi} chooseTrack={chooseTrack} />
 
-<div className="fixed bottom-0 left-0 right-0 z-50">
-  <Player accessToken={acceessToken} trackUri={playingTrack.uri} />
-</div>
+      <div className="fixed bottom-0 left-0 right-0 z-50">
+        <Player accessToken={acceessToken} trackUri={playingTrack.uri} />
+      </div>
     </main>
   )
 }
